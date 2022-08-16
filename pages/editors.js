5e8fe5fed637d30ea731e9b3234650d15dd2ac47
Renata Souza <renata.silva@gentrop.com>
2022-08-16 15:00:15 -0300

    add save plugin

--- pages/editors.js
+++ pages/editors.js
@@ -3,10 +3,10 @@ import { CKEditor } from 'ckeditor4-react';
 
 function Editors() {
   return (
-    <>
+    <form>
       <Editor
         apiKey='rwqbmu7r1py640c26ybql3iuc5vt2mpnlys7i1qfaczf6aqw'
-        initialValue="<p>Conteúdo <strong>inicial</strong> :D</p>"
+        initialValue="<p>Tiny <strong>MCE</strong></p>"
         init={{
           skin: 'snow', // skin padrão compatível com o smartSign
           icons: 'material',
@@ -14,21 +14,25 @@ function Editors() {
           plugins: [
             'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
             'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help'
+            'insertdatetime', 'media', 'table', 'code', 'help', 'save'
           ],
-          toolbar: 'undo redo | blocks | ' +
+          toolbar: 'undo redo | table | blocks | ' +
             'bold italic forecolor fontfamily | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | table code',
+            'alignright alignjustify | bullist numlist | ' +
+            'removeformat | code | save',
           menubar: 'edit view insert format table olar',
           menu: {view: { title: 'View', items: 'code | preview' }, olar: { title: 'Olar!', items: 'code' },},
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px;}',
+          save_onsavecallback: () => {
+            const content = tinymce.activeEditor.getContent();
+            console.log(content);
+          }
         }}
       />
       <CKEditor
-        initData="<p>Hello from CKEditor 4!</p>"
+        initData="<p>CKEditor 4</p>"
       />
-    </>
+    </form>
   );
 }
 
