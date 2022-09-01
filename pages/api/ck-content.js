5d89cb6cd6d71b883ebee80cd262b5870d425ce7
renatasouzas <renata.silva@gentrop.com>
2022-09-01 11:33:50 -0300

    refactor: updateContent functions + add db.json

--- pages/api/ck-content.js
+++ pages/api/ck-content.js
@@ -1,15 +1,3 @@
 let content = '<table border="1"><tbody><tr><td><img alt="" src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=870&amp;q=80" style="height:133px; width:200px" /></td><td><h1>T&iacute;tulo</h1><h2>Subtitulo</h2><pre>Cargo: Pipipi popo</pre></td></tr></tbody></table>'
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(content) 
-  }
-  else if (req.method === 'PATCH') {
-    const newContent = req.body
-    console.log(newContent)
-    content = newContent
-    console.log(content, 'data content')
-    res.status(200).json(newContent)
-  }
-} 
 
--- pages/editors.js
+++ pages/editors.js
@@ -1,22 +1,22 @@
 import { useState, useRef, useEffect } from 'react';
 import TinyMCE from './components/TinyMCE';
 import CKEditor4 from './components/CKEditor4';
-import CKEditor5 from './components/CKEditor5';
+// import CKEditor5 from './components/CKEditor5';
 
 export default function Editors({dbContent}) {
 
   ////////////////////////// CKEditor5
-    const [editorLoaded, setEditorLoaded] = useState(false);
-    const [data, setData] = useState("");
+    // const [editorLoaded, setEditorLoaded] = useState(false);
+    // const [data, setData] = useState("");
 
-    useEffect(() => {
-      setData('batatinha')
-      setEditorLoaded(true);
-    }, []);
+    // useEffect(() => {
+    //   setData('<p>Conteudo CKEditor5</p>')
+    //   setEditorLoaded(true);
+    // }, []);
   /////////////////////////
 
-  const [ tinyContent, setTinyContent ] = useState(dbContent.tinyContent)
-  const [ ckContent, setCkContent ] = useState(dbContent.ckContent)
+  const [ tinyContent, setTinyContent ] = useState(dbContent.tinyMCE)
+  const [ ckContent, setCkContent ] = useState(dbContent.CKEditor4)
 
   function handleTinyChange(editorContent) {
     setTinyContent(editorContent);
@@ -27,19 +27,19 @@ export default function Editors({dbContent}) {
   }
 
   function handleTinySubmit() {
-    let url = 'http://localhost:3000/api/tiny-content'
-    updateContent(url, tinyContent)
+    updateTinyContent(tinyContent)
   }
 
   function handleCkSubmit() {
-    let url = 'http://localhost:3000/api/ck-content'
-    updateContent(url, ckContent)
+    updateCKContent(ckContent)
   }
 
-  const updateContent = async (url, body) => {
-    const response = await fetch(url, {
+  const updateTinyContent = async (body) => {
+    const response = await fetch('http://localhost:5000/content', {
       method: 'PATCH',
-      body: JSON.stringify(body),
+      body: JSON.stringify({
+        tinyMCE: body
+      }),
       headers: {
         'Content-Type': 'application/json'
       }
@@ -48,15 +48,29 @@ export default function Editors({dbContent}) {
     setTinyContent(data)
   }
 
+  const updateCKContent = async (body) => {
+    const response = await fetch('http://localhost:5000/content', {
+      method: 'PATCH',
+      body: JSON.stringify({
+        CKEditor4: body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    setCkContent(data)
+  }
+
   return (
     <>
-      <CKEditor5
+      {/* <CKEditor5
         name="description"
         onChange={(data) => {
           setData(data);
         }}
         editorLoaded={editorLoaded}
-      /> {JSON.stringify(data)}
+      /> {JSON.stringify(data)} */}
       
       <TinyMCE 
         handleSubmit={handleTinySubmit}
@@ -74,17 +88,14 @@ export default function Editors({dbContent}) {
 }
 
 export async function getServerSideProps() {
-  const tinyResponse = await fetch('http://localhost:3000/api/tiny-content')
-  const tinyContent = await tinyResponse.json()
-
-  const ckResponse = await fetch('http://localhost:3000/api/ck-content')
-  const ckContent = await ckResponse.json()
+  const content = await fetch('http://localhost:5000/content')
+  const contentData = await content.json()
 
   return {
     props: {
       dbContent: {
-        "tinyContent": tinyContent, 
-        "ckContent": ckContent
+        "tinyMCE": contentData.tinyMCE, 
+        "CKEditor4": contentData.CKEditor4
       },
     }
   }
